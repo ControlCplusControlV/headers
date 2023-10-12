793f791d6e00657871abe2469e0b9eed9d173936
ControlCplusControlV <randomlikeabledude@gmail.com>
2023-10-12 14:02:11 -0600

    fixed

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ fn main() {
     let input = env::args().collect::<Vec<String>>()[1..].join(" ");
 
     let output = format!(
-        "{}\n#{}{}{}#\n{}",
+        "{}\n{}#{}{}{}#\n{}",
         "    ################################################################",
-        "   ",
-        (0..(64 - input.len()) / 2).map(|_| " ").collect::<String>(),
+        "    ",
+        (0..(62 - input.len()) / 2).map(|_| " ").collect::<String>(),
         input.to_uppercase(),
+        (0..(62 - input.len()) / 2).map(|_| " ").collect::<String>(),
         "    ################################################################"
     );
 
